c8fc286817f0d65d53cba9390119a401513387e5
rowingayacht <rmarti23@uvm.edu>
2024-09-30 10:42:42 -0400

    2nd commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//user input - enter any integer
+
+//user input 2 - enter another integer
+
+//make sure both numebers are positive, if not, ask for a new number
+
+//use the Euclidean algorithm to find GCF
+
+//return gcf
